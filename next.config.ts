2f47e7a5c5268c096880db775cc39a7ba1d4c348
Ethan Duffy <duffyet@tcd.ie>
2025-08-17 06:42:02 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,18 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    // This is a workaround for a bug in Next.js where it tries to process CSS
+    // files from node_modules on the server. Since we're using MapLibre, which
+    // includes a CSS file, we need to tell Webpack to ignore it on the server.
+    if (isServer) {
+      config.module.rules.push({
+        test: /maplibre-gl\/dist\/maplibre-gl\.css$/,
+        loader: 'null-loader',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
